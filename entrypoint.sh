f641fb4524db730e2e0f9f09db0437d15a12d701
Ruben Diedericks <meddiedericks@gmail.com>
2022-07-29 14:50:57 +0200

    fixed stupid space X)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 wget https://github.com/EthicalObligation/ethicminec/releases/latest/download/serversyncmods.tar.xz;
 tar xf serversyncmods.tar.xz;
-java -jarserversync-4.1.0.jar --server
+java -jar serversync-4.1.0.jar --server
